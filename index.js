8cc82a0dfc8b3b030d2fd5780dab73de1bd9d2a1
Jhonny-Almeida <jhonnyalmeida03@gmail.com>
2024-02-09 22:14:22 -0400

    final

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-awawa
+const heroi = "bilu"
+let xp = 36
+if (xp >= 1001 && xp <= 2000) {
+    console.log ("O Herói de nome " + heroi  + " está no nível de bronze")
+}else if (xp >= 2001 && xp <= 5000) {
+    console.log ("O Herói de nome " + heroi  + " está no nível de prata")
+}else if (xp >= 5001 && xp <= 7000) {
+    console.log ("O Herói de nome " + heroi  + " está no nível de ouro")
+}else if (xp >= 7001 && xp <= 8000) {
+    console.log ("O Herói de nome " + heroi  + " está no nível de platina")
+}else if (xp >= 8001 && xp <= 9000) {
+    console.log ("O Herói de nome " + heroi  + " está no nível de Ascendente")
+}else if (xp >= 9001 && xp <= 10000) {
+    console.log ("O Herói de nome " + heroi  + " está no nível de Imortal")
+}else if (xp >= 10001) {
+    console.log ("O Herói de nome " + heroi  + " está no nível de Radiante")
+}else {
+    console.log("O Herói de nome " + heroi  + " está no nível de ferro")
+}
